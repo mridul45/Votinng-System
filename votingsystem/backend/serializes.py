40efd00d7ac65f0e9bb127092439ed52eb59265c
corposhare <workforcorposhare@gmail.com>
2023-11-26 15:21:48 +0530

    c

--- votingsystem/backend/serializes.py
+++ votingsystem/backend/serializes.py
@@ -6,7 +6,7 @@ class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = Candidate
-        fields = ["id","candidate_id","party_affiliated","election_type","bio","photo","campaign_slogan"]
+        fields = "__all__"
 
 
 class CandidateCreateSerializer(serializers.ModelSerializer):
