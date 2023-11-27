e66cfbaa29b7448429d48725f0a1af7337756f23
corposhare <workforcorposhare@gmail.com>
2023-11-27 12:34:32 +0530

    c

--- votingsystem/backend/urls.py
+++ votingsystem/backend/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'candidates',CandidateViewSet,basename='candidate')
 router.register(r'voters',VoterViewset,basename='voter')
+router.register(r'elections',ElectionViewSet,basename='elections')
 
 urlpatterns = [
     path('',include(router.urls))
